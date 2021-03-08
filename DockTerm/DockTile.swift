c9a5de096d75875f52cec85eaaefdd6601dbbd8e
Carson Katri <carson.katri@gmail.com>
2021-03-08 15:42:07 -0500

    Match style of Terminal icon

--- DockTerm/DockTile.swift
+++ DockTerm/DockTile.swift
@@ -10,7 +10,7 @@ struct DockTile: View {
                 Text(output)
                     .layoutPriority(1)
             } else {
-                (Text("›").font(.system(size: 28, weight: .heavy, design: .monospaced)).baselineOffset(-1.5) + Text("\(terminal.input)\(terminal.isFocused && terminal.isShown ? "█" : "")"))
+                (Text("›").font(.system(size: 24, weight: .heavy, design: .monospaced)).baselineOffset(-1.5) + Text("\(terminal.input)\(terminal.isFocused && terminal.isShown ? "█" : "")"))
                     .lineLimit(terminal.output != nil ? 1 : nil)
                     .layoutPriority(1)
             }
@@ -18,10 +18,18 @@ struct DockTile: View {
         }
         .multilineTextAlignment(.leading)
         .foregroundColor(.white)
-        .padding(.horizontal)
-        .padding(.vertical, 8)
-        .font(.system(size: 21, weight: .bold, design: .monospaced))
-        .background(Color.black)
+        .padding(.leading, 12)
+        .padding(.top, 4)
+        .font(.system(size: 18, weight: .bold, design: .monospaced))
+        .background(ZStack {
+            RoundedRectangle(cornerRadius: 18)
+                .stroke(Color.black, lineWidth: 4)
+                .padding(8)
+            RoundedRectangle(cornerRadius: 25)
+                .stroke(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.8809571862, green: 0.9750807881, blue: 1, alpha: 1)), Color(#colorLiteral(red: 0.5036019087, green: 0.493819952, blue: 0.4939008355, alpha: 1))]), startPoint: .top, endPoint: .bottom), lineWidth: 8)
+        })
+        .background(Color(#colorLiteral(red: 0.1490026712, green: 0.1490303874, blue: 0.148996681, alpha: 1)))
         .cornerRadius(25)
+        .padding(10)
     }
 }
